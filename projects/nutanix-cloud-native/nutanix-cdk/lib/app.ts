069c66f52038e8bfd422d8292ab5336a6cec03ad
Abhinav Pandey <abhinavmpandey08@gmail.com>
2022-09-27 00:04:26 -0700

    update nutanix stack

--- projects/nutanix-cloud-native/nutanix-cdk/lib/app.ts
+++ projects/nutanix-cloud-native/nutanix-cdk/lib/app.ts
@@ -3,18 +3,12 @@ import * as cdk from 'aws-cdk-lib';
 import { NutanixInfrastructureStack } from './nutanix-cdk-stack';
 
 const app = new cdk.App();
-new NutanixInfrastructureStack(app, 'NutanixInfrastructureStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: 'account123', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const stack = new NutanixInfrastructureStack(app, 'NutanixInfrastructureStack', {
+  env: {
+    account: process.env.DEPLOYMENT_ACCOUNT_ID,
+    region: process.env.DEPLOYMENT_ACCOUNT_REGION
+  },
 });
+
+console.log(stack.environment)
